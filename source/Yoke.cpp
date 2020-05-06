94840a81866e9a9bfea88b7ef51bde78704905e2
Marcin Sławicz <marcin.slawicz@gmail.com>
2020-05-06 22:17:57 +0200

    IMU I2C device tested OK; Yoke handler call OK

--- source/Yoke.cpp
+++ source/Yoke.cpp
@@ -3,25 +3,55 @@
 Yoke::Yoke(events::EventQueue& eventQueue) :
     eventQueue(eventQueue),
     systemLed(LED1),
-    usbJoystick(USB_VID, USB_PID, USB_VER)
+    usbJoystick(USB_VID, USB_PID, USB_VER),
+    imuInterruptSignal(LSM6DS3_INT1),
+    i2cBus(I2C1_SDA, I2C1_SCL),
+    sensorGA(i2cBus, LSM6DS3_AG_ADD)
 {
     printf("Yoke object created\r\n");
+
+    i2cBus.frequency(400000);
 
     // connect USB joystick
     usbJoystick.connect();
 
-    //Yoke handler is executed every 10 ms
-    eventQueue.call_every(10, callback(this, &Yoke::handler));
+    // configure IMU sensor
+    // INT1<-DRDY_G
+    sensorGA.write((uint8_t)LSM6DS3reg::INT1_CTRL, std::vector<uint8_t>{0x02});
+    // accelerometer ODR=104 Hz, full scale 2g, antialiasing 400 Hz
+    // gyroscope ODR=104 Hz, full scale 500 dps,
+    sensorGA.write((uint8_t)LSM6DS3reg::CTRL1_XL, std::vector<uint8_t>{0x40, 0x44});
+    // gyroscope HPF enable, HPF=0.0081 Hz
+    sensorGA.write((uint8_t)LSM6DS3reg::CTRL7_G, std::vector<uint8_t>{0x40});
+
+    // call handler on IMU interrupt rise signal
+    imuInterruptSignal.rise(callback(this, &Yoke::imuInterruptHandler));
+    // this timeout calls handler for the first time
+    // next calls will be executed upon IMU INT1 interrupt signal
+    imuIntTimeout.attach(callback(this, &Yoke::imuInterruptHandler), 0.1f);
+
+    // start handler timer
+    handlerTimer.start();
 }
 
 
 
 /*
-* yoke handler should be called periodically every 10 ms
+* yoke handler is called on IMU interrupt signal
 */
 void Yoke::handler(void)
 {
+    // this timeout is set only for the case of lost IMU interrupt signal
+    // the timeout should never happen, as the next interrupt should be called earlier
+    imuIntTimeout.attach(callback(this, &Yoke::imuInterruptHandler), 0.02f);
+
+    // measure time elapsed since the previous call
+    float deltaT = handlerTimer.read();
+    handlerTimer.reset();
     counter++;
+
+    // read IMU sensor data
+    auto sensorData = sensorGA.read((uint8_t)LSM6DS3reg::OUT_X_L_G, 12);
 
     // joystick report test
     JoystickData testData;
--- source/Yoke.h
+++ source/Yoke.h
@@ -2,22 +2,43 @@
 #define YOKE_H_
 
 #include "USBJoystick.h"
+#include "I2CDevice.h"
 #include <mbed.h>
 
 #define USB_VID     0x0483 //STElectronics
 #define USB_PID     0x5711 //joystick in FS mode + 1
 #define USB_VER     0x0001 //Nucleo Yoke IMU ver. 1
 
+#define I2C1_SCL    PB_8
+#define I2C1_SDA    PB_9
+
+#define LSM6DS3_AG_ADD  0xD6
+#define LSM6DS3_INT1    PC_8
+
+enum struct LSM6DS3reg : uint8_t
+{
+    INT1_CTRL = 0x0D,
+    CTRL1_XL = 0x10,
+    CTRL7_G = 0x16,
+    OUT_X_L_G = 0x22,
+};
+
 class Yoke
 {
 public:
     Yoke(events::EventQueue& eventQueue);
 private:
+    void imuInterruptHandler(void) { eventQueue.call(callback(this, &Yoke::handler)); }
     void handler(void);
     events::EventQueue& eventQueue;     // event queue of the main thread
     DigitalOut systemLed;               // yoke heartbeat LED
     uint32_t counter{0};                // counter of handler execution
     USBJoystick usbJoystick;            // USB HID joystick device
+    InterruptIn imuInterruptSignal;     //IMU sensor interrupt signal
+    I2C i2cBus;                         // I2C bus for IMU sensor
+    I2CDevice sensorGA;                 // IMU gyroscope and accelerometer sensor
+    Timeout imuIntTimeout;              // timeout of the IMU sensor interrupts
+    Timer handlerTimer;                 // measures handler call period
 };
 
 #endif /* YOKE_H_ */
