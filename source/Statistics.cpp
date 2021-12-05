16cf218ea313ecb894d7d970619ac44e9859fac5
Marcin Sławicz <marcin.slawicz@gmail.com>
2021-12-05 11:00:21 +0100

    statistics refactored

--- source/Statistics.cpp
+++ source/Statistics.cpp
@@ -8,25 +8,25 @@
 #include <mbed.h>
 #include "mbed_stats.h"
 #include "Statistics.h"
+#include <iostream>
 
 #if !defined(MBED_THREAD_STATS_ENABLED)
 #error "Stats not enabled"
 #endif
 
-void listThreads(CommandVector cv)
+void listThreads(CommandVector  /*cv*/)
 {
-    mbed_stats_thread_t* stats = new mbed_stats_thread_t[MAX_THREAD_STATS];
-    int numberOfThreads = mbed_stats_thread_get_each(stats, MAX_THREAD_STATS);
+    auto* stats = new mbed_stats_thread_t[MAX_THREAD_STATS];     //NOLINT(cppcoreguidelines-owning-memory)
+    size_t numberOfThreads = mbed_stats_thread_get_each(stats, MAX_THREAD_STATS);
 
-    printf("ID,Name,State,Priority,Stack size, Stack space\r\n");
-    for(int i = 0; i < numberOfThreads; i++)
+    std::cout << "ID, Name, State, Priority, Stack size, Stack space" << std::endl;
+    for(size_t i = 0; i < numberOfThreads; i++)
     {
-        printf("0x%X,%s,%u,%u,%u,%u\r\n",
-                (unsigned int)stats[i].id,
-                stats[i].name,
-                (unsigned int)stats[i].state,
-                (unsigned int)stats[i].priority,
-                (unsigned int)stats[i].stack_size,
-                (unsigned int)stats[i].stack_space);
+        std::cout << "0x" << std::hex << stats[i].id << ", ";   //NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+        std::cout << stats[i].name << ", ";                     //NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+        std::cout << stats[i].state << ", ";                    //NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+        std::cout << stats[i].priority << ", ";                 //NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+        std::cout << stats[i].stack_size << ", ";               //NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+        std::cout << stats[i].stack_space << std::endl;         //NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
     }
 }
