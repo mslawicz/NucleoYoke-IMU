47e64a8a40e9e1fcaa3309f76a069b272fc4f8fa
Marcin Sławicz <marcin.slawicz@gmail.com>
2021-12-05 11:04:09 +0100

    storage files reverted; build OK

--- source/Storage.h
+++ source/Storage.h
@@ -5,7 +5,6 @@
 #include "KVStore.h"
 #include "kvstore_global_api.h"
 #include "mbed.h"
-#include <iostream>
 #include <string>
 
 /*
@@ -16,17 +15,75 @@ class KvStore
 public:
     KvStore(KvStore const&) = delete;       // do not allow copy constructor of a singleton
     void operator=(KvStore const&) = delete;
-    KvStore(KvStore&&) = delete;
-    void operator=(KvStore&&) = delete;
     static KvStore& getInstance();
-    size_t restoreData(std::string& key, void* pData);
-    static int storeData(std::string& key, const void* pData, size_t size);
-    static void list(const CommandVector& cv);
-    static void clear(const CommandVector& cv);
+    
+    template<typename T> void store(const std::string key, T value)     // store key-value pair in memory
+    {
+        int result = kv_set(key.c_str(), &value, sizeof(T), 0);
+        if(result)
+        {
+            printf("Parameter '%s' store error %d\n", key.c_str(), MBED_GET_ERROR_CODE(result));
+        }
+    }
+
+    /*
+    restore value from the given key
+    if key not found, create the parameter with default value
+    */
+    template<typename T> T restore(const std::string key, T defaultValue)
+    {
+        T value;
+        bool error = false;
+        int result = kv_get_info(key.c_str(), &info);
+        if(result)
+        {
+            error = true;
+            printf("Parameter '%s' get info error %d\n", key.c_str(), MBED_GET_ERROR_CODE(result));
+        }
+        else
+        {
+            size_t actualSize;
+            result = kv_get(key.c_str(), &value, info.size, &actualSize);
+            if(result)
+            {
+                error = true;
+                printf("Parameter '%s' restore error %d\n", key.c_str(), MBED_GET_ERROR_CODE(result));
+            }
+        }
+
+        if(error)
+        {
+            value = defaultValue;
+            store<T>(key, value);
+        }
+
+        return value;
+    }
+
+    /*
+    restore value from the given key using min-max limits
+    if key not found, create the parameter with default value
+    */
+    template<typename T> T restore(const std::string key, T defaultValue, T minValue, T maxValue)
+    {
+        T value = restore<T>(key, defaultValue);
+        if(value > maxValue)
+        {
+            value = maxValue;
+        }
+        else if(value < minValue)
+        {
+            value = minValue;
+        }
+
+        return value;
+    }
+
+    void list(CommandVector cv);
+    void clear(CommandVector cv);
 private:
     KvStore();
-    ~KvStore();
-    kv_info_t info{0, 0};
+    kv_info_t info;
 };
 
 #endif /* STORAGE_H_ */
